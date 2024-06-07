19ee2187062e9a0b8b549e7b8edfc65216a694ec
Jaqea <1601676692@qq.com>
2024-06-07 15:39:42 +0800

    revise debounce and complete throttle

--- debounce/debounce.js
+++ debounce/debounce.js
@@ -8,27 +8,36 @@ const setCount = function () {
 const debounce = function (func, waitTime, immediate) {
   let timoutId, result;
 
-  arguments.callee.cancel = () => {
-    clearTimeout(timoutId);
-    timoutId = null;
-  };
+  const debounced = function () {
+    const args = arguments;
+    const context = this;
 
-  return function () {
     if (timoutId) clearTimeout(timoutId);
     if (immediate) {
       const callNow = !timoutId;
       timoutId = setTimeout(() => {
         timoutId = null;
       }, waitTime);
-      if (callNow) result = func.apply(this, arguments);
+      if (callNow) result = func.apply(context, args);
     } else {
-      timoutId = setTimeout(() => {
-        func.apply(this, arguments);
+      timoutId = setTimeout(function () {
+        result = func.apply(context, args);
       }, waitTime);
     }
     return result;
   };
+
+  debounced.cancel = function () {
+    clearTimeout(timoutId);
+    timoutId = null;
+  };
+
+  return debounced;
 };
 
-ndContainer.addEventListener("mousemove", debounce(setCount, 500, true));
-ndBtn.addEventListener("click", debounce.cancel);
+const setCountAction = debounce(setCount, 500, true);
+
+ndContainer.addEventListener("mousemove", setCountAction);
+ndBtn.addEventListener("click", function () {
+  setCountAction.cancel();
+});
---
+++ throttle/throttle.js
@@ -0,0 +1,116 @@
+let count = 0;
+
+const ndContainer = document.getElementById("js-container");
+const ndBtn = document.getElementById("js-cancel");
+
+const setCount = function () {
+  ndContainer.innerHTML = ++count;
+};
+
+// function throllte(func, wait) {
+//   let previous = 0;
+
+//   return function () {
+//     const now = +new Date();
+//     if (now - previous >= wait) {
+//       previous = +new Date();
+//       func.apply(this, arguments);
+//     }
+//   };
+// }
+
+// function throllte(func, wait) {
+//   let timer;
+
+//   return function () {
+//     if (!timer) {
+//       timer = setTimeout(() => {
+//         func.apply(this, arguments);
+//         clearTimeout(timer);
+//         timer = null;
+//       }, wait);
+//     }
+//   };
+// }
+
+// function throllte(func, wait) {
+//   let previous = 0,
+//     timer;
+
+//   return function () {
+//     const now = +new Date();
+//     const remaining = wait - (now - previous);
+//     if (remaining <= 0 || remaining > wait) {
+//       if (timer) {
+//         clearTimeout(timer);
+//         timer = null;
+//       }
+//       previous = now;
+//       func.apply(this, arguments);
+//     } else if (!timer) {
+//       timer = setTimeout(() => {
+//         previous = +new Date();
+//         func.apply(this, arguments);
+//         timer = null;
+//       }, wait);
+//     }
+//   };
+// }
+
+function throllte(func, waitTime, options = {}) {
+  let context, args, timeoutId, result;
+  previous = 0;
+  if (!options) options = {};
+
+  const later = function () {
+    previous = options.leading ? +new Date() : 0;
+    result = func.apply(context, args);
+    timeoutId = null;
+    if (timeoutId) context = args = null;
+  };
+
+  const throttled = function () {
+    const now = +new Date();
+    if (!previous && !options.leading) previous = now;
+    const remaining = waitTime - (now - previous);
+    console.log(now, previous, remaining);
+    context = this;
+    args = arguments;
+
+    if (remaining <= 0 || remaining > waitTime) {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+        timeoutId = null;
+      }
+
+      previous = now;
+      result = func.apply(context, args);
+
+      if (!timeoutId) {
+        context = args = null;
+      }
+    } else if (!timeoutId && options.trailing) {
+      timeoutId = setTimeout(later, waitTime);
+    }
+
+    return result;
+  };
+
+  throttled.cancel = () => {
+    clearTimeout(timeoutId);
+    timeoutId = null;
+    previous = 0;
+  };
+
+  return throttled;
+}
+
+const setCountAction = throllte(setCount, 2000, {
+  leading: true,
+  trailing: false,
+});
+
+ndContainer.addEventListener("mousemove", setCountAction);
+ndBtn.addEventListener("click", function () {
+  setCountAction.cancel();
+});
